c8b4635975f9c3e6dc056909cdf2d144a907b566
AaronR23 <aaron2612@yahoo.ie>
2024-12-13 19:11:12 +0000

    .

--- cyclo/src/main/java/cyclo/cyclo.java
+++ cyclo/src/main/java/cyclo/cyclo.java
@@ -1,24 +1,28 @@
 package cyclo;
+
 public class cyclo {
-
-    public static void main(String[] args) {
-        // Test the determineTriangle method with example values
-        String result = determineTriangle(0, 0, 0);
-        System.out.println("Result: 1" + result);
-    }
-
-    // Method to determine if the triangle is valid or invalid
-    public static String determineTriangle(int a, int b, int c) {
-        if (a <= 0 || b <= 0 || c <= 0) {
-            return "invalid";
-        } else if(a==b || a==c || b==c) {
-        	return "isoceles";
-        }
-     else if(a+b<=c || a+c<=b || b+c<=a) {
-    	return "isoceles";
-    }
-        else {
-            return "scalene";
-        }
-    }
+	public static String determineTriangle(int a, int b, int c) {
+	    if (a <= 0 || b <= 0 || c <= 0) {
+	        return "Invalid";
+	    }
+	    
+	    ///////////////////
+	    
+	    if (a + b <= c || a + c <= b || b + c <= a) {
+	        return "Not a triangle";
+	    }
+	    
+	    //////////////////////////////
+	    
+	    if (a == b && b == c) {
+	        return "Equilateral";
+	    } else if (a == b || a == c || b == c) {
+	        return "Isosceles";
+	    } else {
+	        return "Scalene";
+	    }	    
+	}
 }
+
+
+
